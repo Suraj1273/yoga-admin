0d3f357036973f17ae8b5657d03abb1d8d4df2ae
suraj <abcsuraj435@gmail>
2022-12-23 17:37:30 +0530

    create imageSlider api of get and post and pending work slider api

--- src/app/Image/view-images/view-images.component.ts
+++ src/app/Image/view-images/view-images.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ServiceService } from 'src/app/services/service.service';
 @Component({
   selector: 'app-view-images',
   templateUrl: './view-images.component.html',
   styleUrls: ['./view-images.component.scss']
 })
 export class ViewImagesComponent implements OnInit {
-
-  constructor() { }
-
+  imagedetails:any = [];
+  constructor(private service:ServiceService) { }
+  
   ngOnInit(): void {
+    this.getSlider(1)
+  }
+
+  getSlider(data:any){
+    this.service.getSlider(data).subscribe((res:any)=>{
+      this.imagedetails =  res.user;
+      console.log(this.imagedetails,'adfsadf')
+    })
   }
 
 }
